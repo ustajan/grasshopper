78c96266d83cae641ccdf10d69d63267adb8d398
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-04-13 14:41:09 -0400

    Update Analysis.hh

--- include/Analysis.hh
+++ include/Analysis.hh
@@ -140,7 +140,7 @@ private:
   G4double theta;
   G4float Time;
   G4int ID;
-  G4int TrackID;
+  unsigned int TrackID;
   G4int EventID;
   G4int ProcID;
   std::vector<double> Ev,Edepv,xv,yv,zv,thetav;
